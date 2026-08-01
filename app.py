5d1ecb76d7138b2628ac4af26470ed96881993dd
yasht9211 <118149620+yasht9211@users.noreply.github.com>
2026-08-01 10:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,6 @@ with tab2:
 
          code = response['messages'][-1].content[-1]['text']
          st.html(code, width="stretch", unsafe_allow_javascript=True)
-
          except Exception as err:
          st.error("Error Code: ", err)
 
