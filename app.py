041ec9f4a8bfc6b686cf0537b3e3e4fce8036bf6
yasht9211 <118149620+yasht9211@users.noreply.github.com>
2026-08-01 10:52:01 +0530

    Fix indentation in exception handling

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ with tab2:
          code = response['messages'][-1].content[-1]['text']
          st.html(code, width="stretch", unsafe_allow_javascript=True)
 
-       except Exception as err:
+         except Exception as err:
          st.error("Error Code: ", err)
 
 with tab3:
